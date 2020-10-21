9a15a464219f1c7e6e7edbd768c0d88969fe8c24
HongTW <bbqlp33@gmail.com>
2020-10-22 00:02:38 +0800

    🚀 Update: Update correct implement for tesing overflow by bitwise-operation

--- practice/bitwise-operation/isAddOK/main.go
+++ practice/bitwise-operation/isAddOK/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func isAddOK(x, y int64) bool {
+func isAddOKLegacy(x, y int64) bool {
 	xSign := (x >> 63) & 1 // x >= 0 ? 0 : 1
 	ySign := (y >> 63) & 1 // y >= 0 ? 0 : 1
 
@@ -15,6 +15,20 @@ func isAddOK(x, y int64) bool {
 	isNegOK := ((((xSign + ySign) - (neg>>63)&1) ^ 3) >> 1) & 1
 
 	return (isPosOK & isNegOK) == 1
+}
+
+func isAddOK(x, y int64) bool {
+	xSign := x >> 63
+	ySign := y >> 63
+	finalSign := (x + y) >> 63
+
+	/*
+		pos + neg ->  xSign ^ xSign = -1
+		pos + pos ->  xSign ^ xSign = 0 and finalSign = 0
+		neg + neg ->  xSign ^ xSign = 0 and finalSign = -1
+	*/
+	return (xSign^ySign == -1) ||
+		(finalSign^xSign == 0 && finalSign^ySign == 0)
 }
 
 func main() {
