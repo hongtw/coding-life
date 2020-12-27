9fd5eefb73d8503f90f105086306c3363517e264
HongTW <bbqlp33@gmail.com>
2020-12-27 23:49:24 +0800

    🚩 Codesignal: Update footballFanIds and use technic of Repetition (greedy and non-greedy) in regex
    Golang regex 不支援 lookahead 語法，因此使用 greedy and non-greedy 達到類似操作

--- codesingal/footballFanIds/footballFanIds.go
+++ codesingal/footballFanIds/footballFanIds.go
@@ -1,5 +1,13 @@
 // https://app.codesignal.com/challenge/L9YzWMLMJbwSTGRkk
 
+// Repetition (greedy and non-greedy) https://yourbasic.org/golang/regexp-cheat-sheet/#repetition-greedy-and-non-greedy
+// Regexp	Meaning
+// x*	    zero or more x, prefer more
+// x*?	    prefer fewer (non-greedy)
+// x+	    one or more x, prefer more
+// x+?	    prefer fewer (non-greedy)
+// x?	    zero or one x, prefer one
+// x??	    prefer zero
 package main
 
 import (
@@ -22,14 +30,16 @@ func main() {
 	root := "/root/devops/"
 	invites := readFile(root + "invite.info")
 	bans := readFile(root + "ban.info")
-	invitePat := regexp.MustCompile(
-		fmt.Sprintf(`/(%v).*\.info$`, strings.Join(invites, "|")))
-	banPat := regexp.MustCompile(
-		fmt.Sprintf(`/(%v)/\w*\.info$`, strings.Join(bans, "|")))
+	pattern := regexp.MustCompile(fmt.Sprintf(
+		`/(%v)*((%v).*)??/\w+\.info$`,
+		strings.Join(bans, "|"),
+		strings.Join(invites, "|"),
+	))
 
 	fanIds := make([]string, 0)
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if (!banPat.MatchString(path)) && invitePat.MatchString(path) {
+		matched := pattern.FindStringSubmatch(path)
+		if len(matched) > 0 && matched[3] != "" {
 			fanIds = append(fanIds, readFile(path)...)
 		}
 		return nil
