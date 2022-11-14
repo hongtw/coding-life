b364af2921eff4a62d920d58925f5ad3928910de
hongTW <chih.hong.li.@gmail.com>
2022-11-15 01:58:31 +0800

    Update solution for leetecode #53

--- leetcode/0053.Maximum-Subarray/MaximumSubarray.py
+++ leetcode/0053.Maximum-Subarray/MaximumSubarray.py
@@ -12,3 +12,22 @@ class Solution:
             if subsum < 0:
                 subsum = 0
         return maxsum
+
+    # https://leetcode.com/problems/maximum-subarray/discuss/1595195/C%2B%2BPython-7-Simple-Solutions-w-Explanation-or-Brute-Force-%2B-DP-%2B-Kadane-%2B-Divide-and-Conquer
+    def maxSubArrayV2(self, nums: List[int]) -> int:
+        # Time Complexity : O(N), for iterating over nums once
+        # Space Complexity : O(1), only constant extra space is being used.
+        cur_max = 0
+        res_max = 0
+        for n in nums:
+            cur_max = max(n, cur_max+n)
+            res_max = max(res_max, cur_max)
+        return res_max
+            
+    def maxSubArrayV3(self, nums: List[int]) -> int:
+        # Time Complexity : O(N), for iterating over nums once
+        # Space Complexity : O(1)
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i], nums[i-1]+nums[i])
+        return max(nums)
+            
